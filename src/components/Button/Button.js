62cc9b808ed59ef64a155556c49684aeda42e735
Sachin Saini <iamsainisachin@gmail.com>
2019-01-08 05:52:52 +0530

    add initial button styling

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -1,5 +1,24 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const StyledButton = styled.button`
+	border: none;
+	padding: 1em;
+	border-radius: 3px;
+	cursor: pointer;
+	font-weight: bold;
+	font-size: 0.8em;
+	background-color: ${props => (props.search ? '#1982C4' : '#ffca3a')};
+	color: ${props => (props.search ? '#ffffff' : '#0f0f0f')};
+	:hover {
+		opacity: 0.8;
+	}
+`;
 
 export default function Button(props) {
-	return <button onClick={props.clicked}>{props.children}</button>;
+	return (
+		<StyledButton onClick={props.clicked} {...props}>
+			{props.children}
+		</StyledButton>
+	);
 }
