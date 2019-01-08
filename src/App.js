1b7a6190c690c639d473290b1de7998715079c8c
Sachin Saini <iamsainisachin@gmail.com>
2019-01-09 05:28:02 +0530

    add initial weather handler function

--- src/App.js
+++ src/App.js
@@ -1,21 +1,47 @@
 import React, { Component } from 'react';
 import Searchbar from './components/Searchbar/Searchbar';
 import Button from './components/Button/Button';
+import * as config from './config';
 
 class App extends Component {
-	showPosition = position => {
+	state = {
+		type: '',
+		temp: null,
+		lat: null,
+		lon: null
+	};
+	componentDidMount() {
+		this.getLocation();
+	}
+	handleWeatherFetch = () => {
+		fetch(
+			`http://api.openweathermap.org/data/2.5/weather?lat=${
+				this.state.lat
+			}&lon=${this.state.lon}&APPID=${config.API_KEY}`
+		).then(res => {
+			res.json().then(data => {
+				console.log(Math.floor(data.main.temp - 273.15));
+			});
+		});
+	};
+	setPosition = position => {
 		console.log(position.coords.latitude);
 		console.log(position.coords.longitude);
+		this.setState({
+			...this.state,
+			lat: position.coords.latitude,
+			lon: position.coords.longitude
+		});
 	};
 	getLocation = () => {
-		navigator.geolocation.getCurrentPosition(this.showPosition);
+		navigator.geolocation.getCurrentPosition(this.setPosition);
 	};
 	render() {
 		return (
 			<div>
 				<Searchbar />
 				<Button>Search</Button>
-				<Button clicked={this.getLocation} search>
+				<Button clicked={this.handleWeatherFetch} search>
 					Get Current Weather
 				</Button>
 			</div>
