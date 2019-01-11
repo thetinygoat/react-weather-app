947c3cd39021d38e85dd5b3add1a853895710fea
Sachin Saini <iamsainisachin@gmail.com>
2019-01-11 18:26:40 +0530

    add weather info component

--- src/components/WeatherInfo/WeatherInfo.js
+++ src/components/WeatherInfo/WeatherInfo.js
@@ -1,5 +1,21 @@
 import React from 'react';
 
-export default function WeatherInfo() {
-	return <div>weatherInfo</div>;
+export default function WeatherInfo(props) {
+	let icon = null;
+	if (props.icon !== null) {
+		icon = (
+			<img
+				src={`http://openweathermap.org/img/w/${props.icon}.png`}
+				alt="weather icon"
+			/>
+		);
+	}
+	return (
+		<div>
+			{icon}
+			<h1>{props.temp}</h1>
+			<h3>{props.place}</h3>
+			<h3>{props.type}</h3>
+		</div>
+	);
 }
