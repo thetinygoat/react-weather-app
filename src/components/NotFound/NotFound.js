b283ebf332fa63f1d6a3f24ad1c0ed3207636847
Sachin Saini <iamsainisachin@gmail.com>
2019-01-12 18:34:20 +0530

    style not found graphic

--- src/components/NotFound/NotFound.js
+++ src/components/NotFound/NotFound.js
@@ -1,9 +1,16 @@
 import React from 'react';
+import notFound from './notfound.png';
+import styled from 'styled-components';
+const Image = styled.img`
+	width: 25%;
+`;
 
 export default function NotFound() {
 	return (
 		<div>
-			<p>not found</p>
+			<p>
+				<Image src={notFound} alt="not found" />
+			</p>
 		</div>
 	);
 }
